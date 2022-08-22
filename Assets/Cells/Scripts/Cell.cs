e16ae48311323b6e5fdf57047864834b6dc430a7
Juan Payán <juandavidPF@gmail.com>
2022-08-22 16:45:00 -0500

    Started working on Re-Coding cells behaviour

---
+++ Assets/Cells/Scripts/Cell.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Ozamanas.Board
+{
+    public class Cell : MonoBehaviour
+    {
+
+
+    }//Closes Cell class
+}//Closes Namespace declaration
