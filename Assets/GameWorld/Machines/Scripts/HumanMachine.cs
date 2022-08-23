32fdddedf25a1323d9e860a405c2bd5a434d701d
hmax82 <47353129+hmax82@users.noreply.github.com>
2022-08-22 22:32:54 -0500

    Machine Movement AI

---
+++ Assets/GameWorld/Machines/Scripts/HumanMachine.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HumanMachine : MonoBehaviour
+{
+    public MachineState  machine_status;
+    public GameObject flag;
+
+    public void SetMachineStatus(MachineState status)
+    { machine_status = status;}
+
+    public void SetBlockedStatus()
+    {
+        flag.SetActive(true);
+    }
+
+}
---
+++ Assets/GameWorld/Machines/Scripts/MachineState.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "new HumanMachineState", menuName = "ScriptableObjects/Human Machines/Machine State")]
+public class MachineState : ScriptableObject
+{
+    public string status_Name;
+}//Closes HumanMachineState scriptableObject
