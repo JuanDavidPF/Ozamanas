c22eea3166f95dd0b199346a97a76c89dedd8531
Juan Payán <juandavidPF@gmail.com>
2022-09-01 06:16:03 -0500

    Started working on energy pool

---
+++ Assets/GameWorld/Board/Cells/Energy Pool/Scripts/EnergyPool.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Ozamanas.Energy
+{
+    public class EnergyPool : MonoBehaviour
+    {
+
+        [SerializeField] private Transform energyPool;
+    }//Closes EnergyPool class
+}//Closes Namespace declaration
