c856c7eae4b810c614c11d037d25c79f772a2d8b
Juan Payán <juandavidPF@gmail.com>
2022-10-11 01:07:38 -0500

    Fixed industry rotation

---
+++ Assets/Scripts/Helpers/SelectionBase.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace JuanPayan.Helpers
+{
+
+    [SelectionBase]
+    public class SelectionBase : MonoBehaviour
+    {
+
+    }
+}
