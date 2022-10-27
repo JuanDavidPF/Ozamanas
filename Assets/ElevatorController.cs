c495e6eea7b74c253bbf0fe36687d5f92f256918
Juan Payán <juandavidPF@gmail.com>
2022-10-27 14:30:16 -0500

    Finished Merge

--- Assets/ElevatorController.cs
+++ Assets/ElevatorController.cs
@@ -8,44 +8,44 @@ namespace Ozamanas.Board
 
     [RequireComponent(typeof(Collider))]
     [RequireComponent(typeof(Animator))]
-public class ElevatorController : MonoBehaviour
-{
+    public class ElevatorController : MonoBehaviour
+    {
 
-      private Animator animator;
-      private bool gatesOpened = false;
-      private bool machineActivated = false;
+        private Animator animator;
+        private bool gatesOpened = false;
+        private bool machineActivated = false;
 
-      private MachinePhysicsManager currentMachine = null; 
+        private MachinePhysicsManager currentMachine = null;
 
 
-      void Awake()
+        void Awake()
         {
             animator = GetComponent<Animator>();
         }
-    // Start is called before the first frame update
-    public void OpenGates()
-    {
-        if(gatesOpened) return;
-        animator.SetBool("OpenGates",true);
-        gatesOpened = true;
-    }
-    
-    public void ElevatorIsUP()
-    {
-        if(!currentMachine) return;
-        currentMachine.ActivatePhysics(true);
-    }
-    public void CloseGates()
-    {
-        if(!gatesOpened) return;
-        animator.SetBool("OpenGates",false);
-        gatesOpened = false;
-    }
+        // Start is called before the first frame update
+        public void OpenGates()
+        {
+            if (gatesOpened) return;
+            animator.SetBool("OpenGates", true);
+            gatesOpened = true;
+        }
 
-     private void OnTriggerExit(Collider other)
+        public void ElevatorIsUP()
+        {
+            if (!currentMachine) return;
+            currentMachine.SetPhysical();
+        }
+        public void CloseGates()
+        {
+            if (!gatesOpened) return;
+            animator.SetBool("OpenGates", false);
+            gatesOpened = false;
+        }
+
+        private void OnTriggerExit(Collider other)
         {
             if (other.tag != "Machine") return;
-           
+
             CloseGates();
             machineActivated = false;
             currentMachine = null;
@@ -55,19 +55,19 @@ public class ElevatorController : MonoBehaviour
         {
             if (other.tag != "Machine") return;
 
-            if(!gatesOpened) return;
+            if (!gatesOpened) return;
 
             if (machineActivated) return;
 
-             if (other.TryGetComponentInParent(out Machines.MachinePhysicsManager machine))
+            if (other.TryGetComponentInParent(out Machines.MachinePhysicsManager machine))
             {
                 currentMachine = machine;
-                currentMachine.ActivatePhysics(false);
+                currentMachine.SetIntelligent();
                 machineActivated = true;
             }
             else return;
 
         }
-    
-}
+
+    }
 }
