7e6be7e6173800681fa0a6b9a0451497ad25ace9
hmax82 <47353129+hmax82@users.noreply.github.com>
2022-10-28 14:39:14 -0500

    Boss partial implementation

---
+++ Assets/GameWorld/Machines/Scripts/HumanMachineBulldozerBoss.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Ozamanas.Machines
+{
+    public class HumanMachineBulldozerBoss : HumanMachine
+    {
+        public void Attack()
+        {
+
+        }
+
+         public UnityEvent OnAttack;
+    }
+}
