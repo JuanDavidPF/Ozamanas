508712c4d09dda2e91d9054f68642fe5dcd02997
Juan Payán <juandavidPF@gmail.com>
2022-11-11 14:37:32 -0500

    Modifications

---
+++ Assets/Scripts/Helpers/SoundPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Stem;
+using UnityEngine;
+
+public class SoundPlayer : MonoBehaviour
+{
+    [SoundID, SerializeField]
+    internal Stem.ID sound = Stem.ID.None;
+
+    [SerializeField]
+    internal Transform spawnTarget = null;
+
+
+    public void Play()
+    {
+        if (spawnTarget) SoundManager.Play3D(sound, spawnTarget.position);
+        else SoundManager.Play(sound);
+
+    }
+}//Closes SoundPlayer class
