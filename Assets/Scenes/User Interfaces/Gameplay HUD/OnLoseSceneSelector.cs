f079f1ba71978f75a2b4feb5df49f8cd1bd28de8
hmax82 <47353129+hmax82@users.noreply.github.com>
2022-11-27 19:21:03 -0500

    Narrative Functions

---
+++ Assets/Scenes/User Interfaces/Gameplay HUD/OnLoseSceneSelector.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using JuanPayan.Helpers;
+using Ozamanas.Levels;
+using Ozamanas.SaveSystem;
+
+public class OnLoseSceneSelector : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private PlayerDataHolder currentSaveState;
+     [SerializeField] private SceneSwitcher sceneSwitcher;
+    void Awake()
+    {
+        if(!sceneSwitcher) sceneSwitcher = GetComponent<SceneSwitcher>();
+    }
+
+
+    public void SwitchToScene()
+    {
+       LevelHolder temp = GameObject.FindGameObjectWithTag("SceneController").GetComponentInChildren<LevelHolder>();
+       if(temp) sceneSwitcher.SceneToLoad =  temp.levelSelected.level.ifLoseReturnToScene;
+       if(!string.IsNullOrEmpty(temp.levelSelected.level.ifLoseSetDialogue)) currentSaveState.data.currentDialogueCode = temp.levelSelected.level.ifLoseSetDialogue;
+       sceneSwitcher.SetSingleMode();
+       sceneSwitcher.Behaviour();
+
+    }
+}
