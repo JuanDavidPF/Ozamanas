199d7e8480f4b176d23a8c874e43ab78c775474e
hmax82 <47353129+hmax82@users.noreply.github.com>
2023-05-11 16:57:52 -0500

    New levels and Bulldozer Lvl 1,2 & 3

---
+++ Assets/Scripts/Helpers/CameraMiniMap.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+using UnityEngine.InputSystem;
+
+namespace JuanPayan.Helpers
+{
+    [RequireComponent(typeof(CinemachineVirtualCamera))]
+    public class CameraMiniMap : MonoBehaviour
+    {
+        
+        [Tooltip("Vector2 action for rotation movement")]
+        [SerializeField] private InputActionReference _rotationInput;
+
+        [Tooltip("Vector2 action for movement")]
+        [SerializeField] private InputActionReference _movementInput;
+
+        [SerializeField] private InputActionReference _zoomInput;
+
+        [SerializeField] private float anchorSpeed;
+        [SerializeField] private float rotationSpeed;
+
+        [SerializeField] private Vector2 yLimits;
+
+        [SerializeField] private Vector2 xLimits;
+
+        [SerializeField] private Vector2 zLimits;
+
+
+        private Transform _t;
+
+        private void Awake()
+        {
+            _t = transform;
+
+        }//Closes Awake method
+
+        private void OnEnable()
+        {
+            _movementInput.action.Enable();
+            _rotationInput.action.Enable();
+            _zoomInput.action.Enable();
+        }//Closes OnEnable method
+
+        private void Update()
+        {
+            float zoom = _zoomInput.action.ReadValue<float>();
+            Vector2 movementAxis = _movementInput.action.ReadValue<Vector2>();
+            Vector3 movementDirection = new Vector3(movementAxis.x, 0, movementAxis.y).normalized;
+            Vector3 newPosition = _t.position;
+            Vector3 newRotation = new Vector3(0, _rotationInput.action.ReadValue<Vector2>().x, 0);
+
+            newPosition += transform.up * movementAxis.y * Time.deltaTime * anchorSpeed;
+            newPosition += transform.right * movementAxis.x * Time.deltaTime * anchorSpeed;
+            newPosition += transform.forward * zoom * Time.deltaTime * anchorSpeed;
+
+            newPosition = new Vector3(Mathf.Clamp(newPosition.x,xLimits.x,xLimits.y),Mathf.Clamp(newPosition.y,yLimits.x,yLimits.y),Mathf.Clamp(newPosition.z,zLimits.x,zLimits.y));
+
+            _t.position = newPosition;
+             transform.Rotate( newRotation * rotationSpeed * Time.deltaTime, Space.World);
+
+           
+
+        }//Closes UpdatePosition method
+        private void OnDisable()
+        {
+            _movementInput.action.Disable();
+            _rotationInput.action.Disable();
+            _zoomInput.action.Disable();
+
+        }//Closes OnDisable method
+    }
+}
